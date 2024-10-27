118f9737b517f23be867b7f294de378876bcac41
Andrew <andrei.sp.93@gmail.com>
2024-10-27 19:58:00 +0300

    feat: finish the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currentGuess = 0;
+    }
 
+    // Устанавливаем диапазон
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
+    // Делаем предположение (середина диапазона)
     guess() {
-
+        this.currentGuess = Math.ceil((this.min + this.max) / 2);
+        return this.currentGuess;
     }
 
+    // Если предположение больше ответа, сдвигаем верхнюю границу
     lower() {
-
+        this.max = this.currentGuess;
     }
 
+    // Если предположение меньше ответа, сдвигаем нижнюю границу
     greater() {
-
+        this.min = this.currentGuess;
     }
 }
 
